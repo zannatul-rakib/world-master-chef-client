260e4d0252e84da8065fe884c07a2cdb344ce898
zrakib-dev <dev.zrakib@gmail.com>
2023-06-22 04:05:54 +0600

    hero section Edit

---
+++ src/Pages/Home/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import heroImage from "../../../assets/hero.jpg";
+
+const Hero = () => {
+    return (
+        <div className='grid justify-center items-center'>
+            <div className="py-32 my-4">
+  <div className="flex flex-col md:flex-row-reverse  md:justify-between md:items-center gap-8">
+    <img src={heroImage} className="max-w-sm lg:max-w-xl rounded-lg shadow-2xl" />
+    <div>
+      <h1 className="text-3xl lg:text-5xl font-bold">Box Office News!</h1>
+      <p className="py-6 max-w-[500px]">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Hero;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from '../Banner/Banner';
+import Hero from '../Hero/Hero';
 
 const Home = () => {
    
     return (
         <div className='lg:mx-20 my-8'>
             <Banner />
+            <Hero/>
         </div>
     );
 };
