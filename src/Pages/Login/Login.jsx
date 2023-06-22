94b45954685fe67ae0102ad916d8472737657ab4
zrakib-dev <dev.zrakib@gmail.com>
2023-06-22 22:11:54 +0600

    sign in auth complete

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../provider/AuthProvider/AuthProvider';
 
 const Login = () => {
+    const { signIn } = useContext(AuthContext);
+
+    const handleSignIn = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        signIn(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => console.log(error))
+    }
     return (
-        <div>
-            <h2>This is login page</h2>
-            <Link to="/signup">Sign Up</Link>
+        <div className='flex justify-center items-center min-h-screen'>
+            <div className='w-1/3 mx-auto border px-10 py-5 rounded'>
+            <h4 className='text-accent text-center mb-10 text-2xl font-semibold'>Sign In Now</h4>
+                
+<form onSubmit={handleSignIn}>
+  <div className="mb-6">
+    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+    <input type="email" name='email' id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+  </div>
+  <div className="mb-6">
+    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+    <input type="password" name='password' id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+  </div>
+  <div className="flex items-start mb-6">
+    <div className="flex items-center h-5">
+      <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" />
+    </div>
+    <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+  </div>
+  <button type="submit" className="text-white btn btn-primary">Sign In</button>
+</form>
+                <div>
+                    <p className='my-3'>have'nt account please <Link to="/signup" className='btn-link text-primary'>Sign Up</Link></p>
+        </div>
+            </div>
         </div>
     );
 };
